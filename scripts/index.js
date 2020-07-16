80678dcf817111b9e751944170c535d278340914
Lia Cicati <Lia@Alexeis-MacBook-Pro.local>
2020-07-17 00:27:41 +0300

    fixed: added const

--- scripts/index.js
+++ scripts/index.js
@@ -27,33 +27,33 @@ const closePopupImage = popupImage.querySelector(".popup__close-button");
 
 const initialCards = [{
         name: 'Хиби́ны',
-        link: 'https://images.unsplash.com/photo-1518675013095-1cef34ac232e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
-        alt: 'Хиби́ны'
+        link: 'https://images.unsplash.com/photo-1518675013095-1cef34ac232e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'
+
     },
     {
         name: 'Башкирия',
-        link: 'https://images.unsplash.com/photo-1482355383105-63c565658bab?ixlib=rb-1.2.1&auto=format&fit=crop&w=2214&q=80',
-        alt: 'Башкирия'
+        link: 'https://images.unsplash.com/photo-1482355383105-63c565658bab?ixlib=rb-1.2.1&auto=format&fit=crop&w=2214&q=80'
+
     },
     {
         name: 'Ольхо́н',
-        link: 'https://images.unsplash.com/photo-1490879112094-281fea0883dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-        alt: 'Ольхо́н'
+        link: 'https://images.unsplash.com/photo-1490879112094-281fea0883dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+
     },
     {
         name: 'Магадан',
-        link: 'https://images.unsplash.com/photo-1570340831042-040b3999690c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80',
-        alt: 'Магадан'
+        link: 'https://images.unsplash.com/photo-1570340831042-040b3999690c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80'
+
     },
     {
         name: 'Озеро Байкал',
-        link: 'https://images.unsplash.com/photo-1551844931-9c422b3f8a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=935&q=80',
-        alt: 'Озеро Байкал'
+        link: 'https://images.unsplash.com/photo-1551844931-9c422b3f8a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=935&q=80'
+
     },
     {
         name: 'Алтай',
-        link: 'https://images.unsplash.com/photo-1494791286225-ea86fc957ba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3294&q=80',
-        alt: 'Алтай'
+        link: 'https://images.unsplash.com/photo-1494791286225-ea86fc957ba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3294&q=80'
+
     }
 ];
 
@@ -106,8 +106,8 @@ initialCards.forEach(item => (addCard(item.name, item.link)));
 function formSubmitHandlerCard(evt) {
     evt.preventDefault();
 
-    let name = inputTitle.value;
-    let link = inputLink.value;
+    const name = inputTitle.value;
+    const link = inputLink.value;
 
     addCard(name, link);
 
